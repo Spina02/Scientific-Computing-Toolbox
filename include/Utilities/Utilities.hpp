897a94233fffdb7602d74f5991478f3d8a2cc713
Spina02 <47366784+Spina02@users.noreply.github.com>
2024-12-07 15:17:20 +0100

    disabled debug flag

--- include/Utilities/Utilities.hpp
+++ include/Utilities/Utilities.hpp
@@ -7,6 +7,8 @@
 #include <Eigen/Dense>
 
 namespace ScientificToolbox {
+    inline const bool DEBUG = false;
+
     using scalar_func = std::function<double(double, double)>;
     using vec_func = std::function<Eigen::VectorXd(double, const Eigen::VectorXd&)>;
     using var_func = std::variant<scalar_func, vec_func>;
@@ -16,8 +18,6 @@ namespace ScientificToolbox {
     using vec_s = std::vector<std::string>;
     using var_vecs = std::vector<var_vec>;
     using var_expr = std::variant<std::string, vec_s>;
-
-    inline const bool DEBUG = true;
 }
 
 #endif // UTILITIES_HPP
