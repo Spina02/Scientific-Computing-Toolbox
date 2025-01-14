1e3417cf43a6509a9a1e4664de4da6e369843428
christianfaccio <christianfaccio@outlook.it>
2025-01-14 19:30:52 +0100

    fixed some bugs in interpolation and added documentation for interpolation

--- main/Interpolation_Module_main.py
+++ main/Interpolation_Module_main.py
@@ -19,4 +19,22 @@ os.makedirs(output_dir, exist_ok=True)
 # Usage
 if __name__ == "__main__":
     interpolator = interpolation.Interpolator()
-    interpolator.run()
+    data_file = input("Enter the name of the data file: ")
+    if not os.path.exists(os.path.join(data_dir, data_file)):
+        print("File not found.")
+        sys.exit(1)
+    if data_file == "":
+        data_file = "random_data.csv"
+    data = pd.read_csv(os.path.join(data_dir, data_file))
+    interpolator.get_data(data)
+    method = input("Enter the interpolation method (\'linear\', \'lagrange\', \'newton\', \'spline\'): ")
+    if method == "":
+        method = "linear"
+    interpolator.set_interpolation_method(method)
+    value = input("Enter the value to interpolate: ")
+    if value == "":
+        value = data['x'].mean()
+    value = float(value)
+    interpolator.set_value_to_interpolate(value)
+    interpolator.interpolate()
+    interpolator.plot()
