caa2b7dde5fb271fee73b763321606fca7a2d29e
christianfaccio <christianfaccio@outlook.it>
2025-01-14 18:32:16 +0100

    added interpolation test

--- tests/Interpolation_Test.py
+++ tests/Interpolation_Test.py
@@ -1,15 +1,79 @@
-import os
+import unittest
 import sys
+import os 
+import time 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import time
+
 from scipy.interpolate import interp1d, lagrange as scipy_lagrange, CubicSpline
-from scientific_toolbox.interpolation import InterpolationAnalysis, df_to_set_of_points
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Usage
-if __name__ == "__main__":
-    print("\n\nmoved to notebook\n\n")
-    # TODO
+import scientific_toolbox.interpolation as interp
+
+class TestInterpolation(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def test_random_data_generator(self):
+        analyzer = interp.InterpolationAnalysis()
+        random_data = analyzer.random_data_generator(0, 10, 20, seed=17)
+        self.assertEqual(random_data.shape, (20, 2))
+
+    def test_data_generator(self):
+        analyzer = interp.InterpolationAnalysis()
+        data = analyzer.data_generator(0, 10, 20, np.sin)
+        self.assertEqual(data.shape, (20, 2))
+    
+    def test_interpolate_and_plot(self):
+        analyzer = interp.InterpolationAnalysis()
+        random_data = analyzer.random_data_generator(0, 10, 20, seed=17)
+        analyzer.interpolate_and_plot(random_data)
+        self.assertTrue(True)
+
+    def test_interpolation_on_function(self):
+        analyzer = interp.InterpolationAnalysis()
+        function_data = analyzer.data_generator(0, 10, 20, np.sin)
+        function_sparse_data = analyzer.sample_values(function_data['x'], 5)
+        analyzer.interpolate_and_plot(function_data, function_sparse_data)
+        self.assertTrue(True)
+
+    def test_accuracy(self):
+        analyzer = interp.InterpolationAnalysis()
+        function_data = analyzer.data_generator(0, 10, 20, np.sin)
+        function_sparse_data = analyzer.sample_values(function_data['x'], 5)
+        analyzer.analyze_accuracy(function_data, function_sparse_data)
+        self.assertTrue(True)
+    
+    def test_efficiency(self):
+        analyzer = interp.InterpolationAnalysis()
+        function_data = analyzer.data_generator(0, 10, 20, np.sin)
+        function_sparse_data = analyzer.sample_values(function_data['x'], 5)
+        analyzer.analyze_efficiency(function_data, function_sparse_data)
+        self.assertTrue(True)
+    
+    def test_order_of_convergence(self):
+        analyzer = interp.InterpolationAnalysis()
+        function_data = analyzer.data_generator(0, 10, 20, np.sin)
+        function_sparse_data = analyzer.sample_values(function_data['x'], 5)
+        n_points_vector = [4, 8, 16, 32, 64]
+        analyzer.plot_order_of_convergence(function_data, function_sparse_data, n_points_vector)
+        self.assertTrue(True)
+
+    def test_time_comparison(self):
+        analyzer = interp.InterpolationAnalysis()
+        random_data = analyzer.random_data_generator(0, 10, 20, seed=17)
+        analyzer.time_comparison(random_data)
+        self.assertTrue(True)
+    
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
+
