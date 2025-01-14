1156b51f6985d8a383b612f3a529f93e6a048a20
Spina02 <47366784+Spina02@users.noreply.github.com>
2025-01-14 23:53:43 +0100

    added cin inputs

--- main/ODE_Module_main.cpp
+++ main/ODE_Module_main.cpp
@@ -13,24 +13,16 @@ using namespace ScientificToolbox::ODE;
 int main(int argc, char** argv) {
     try {
         // Default values
-        path project_dir = std::filesystem::current_path();//.parent_path();
+        path project_dir = std::filesystem::current_path();
         path inputFile = project_dir / path("data") / path("ode_examples.csv");
         path outputFolder = project_dir / path("output");
 
-        // Parse command line arguments
-        if (argc > 1) {
-            inputFile = project_dir / path("data") / path(std::string(argv[1]));
+        std::cout << "Enter input filename from data folder (press Enter for default):\n";
+        std::string userInput;
+        std::getline(std::cin, userInput);
+        if (!userInput.empty()) {
+            inputFile = project_dir / "data" / userInput;
         }
-        else if (argc > 2) {
-            outputFolder = project_dir / path(std::string(argv[2]));
-        }
-        else if (argc > 3) {
-            std::cout << "\nToo many arguments. Using only the first two arguments." << std::endl;
-        }
-        else {
-            std::cout << "\nNo args entered. Using " << inputFile << " as input file and " << outputFolder << " as output folder..." << std::endl;
-        }
-
 
         std::cout << "ODE Module Demo\n" << std::endl;
 
@@ -56,12 +48,18 @@ int main(int argc, char** argv) {
 
             std::cout << std::endl << "  Solving ODE using different methods:" << std::endl << std::endl;
             
+            // reset files
+            std::ofstream(outputFolder / "ODe_Module" / "euler_solution.csv", std::ios::trunc);
+            std::ofstream(outputFolder / "ODe_Module" / "midpoint_solution.csv", std::ios::trunc);
+            std::ofstream(outputFolder / "ODe_Module" / "rk4_solution.csv", std::ios::trunc);
+
             // Solve using Forward Euler
             auto euler = ForwardEulerSolver(test.expr, y0, t0, tf, h);
             auto euler_solution = euler.solve();
             std::cout << "  Forward Euler Method:\t\t";
             // use the result field to access the final value
             std::cout << euler_solution.get_result() << std::endl;
+            save_to_csv(outputFolder / "euler_solution.csv", euler_solution, true);
 
             // Solve using Explicit Midpoint
             auto midpoint = ExplicitMidpointSolver(test.expr, y0, t0, tf, h);
@@ -69,6 +67,7 @@ int main(int argc, char** argv) {
             std::cout << "  Explicit Midpoint Method:\t";
             // use the result field to access the final value
             std::cout << midpoint_solution.get_result() << std::endl;
+            save_to_csv(outputFolder / "midpoint_solution.csv", midpoint_solution, true);
 
             // Solve using RK4
             auto rk4 = RK4Solver(test.expr, y0, t0, tf, h);
@@ -76,104 +75,9 @@ int main(int argc, char** argv) {
             std::cout << "  Runge-Kutta 4th Order Method:\t";
             // use the result field to access the final value
             std::cout << rk4_solution.get_result() << std::endl;
+            save_to_csv(outputFolder / "rk4_solution.csv", rk4_solution, true);
         }
 
-        /*
-        // You can also define the test cases manually and parse them from strings
-        std::cout << std::endl << "Manual Test Cases" << std::endl;
-        // Example 1: Scalar ODE (dy/dt = y, y(0) = 1)
-        std::cout << std::endl << "Example 1: Scalar ODE\n";
-        var_expr scalar_expr = "y";  // dy/dt = y
-        
-        double t0 = 0.0;
-        double tf = 1.0;
-        double h = 0.001;
-        var_vec y0 = 1.0;  // Initial condition y(0) = 1
-
-        // Print problem description
-        std::cout << "  Scalar ODE:" << std::endl;
-        std::cout << "    dy/dt           :\t" << scalar_expr << std::endl;
-        std::cout << "    Time interval   :\t[" << t0 << ", " << tf << "]" << std::endl;
-        std::cout << "    Step size       :\t" << h << std::endl;
-        std::cout << "    y(0)            :\t" << y0 << std::endl;
-
-        // Solve using Forward Euler
-        std::cout << " Forward Euler Method" << std::endl << std::endl;
-        auto euler = ForwardEulerSolver(scalar_expr, y0, t0, tf, h);
-        auto euler_solution = euler.solve(); // solve without measuring execution time
-        //auto euler_solution = solve_and_measure_execution_time(euler); // solve and measure execution time
-        // Print the trajectory using 2 steps
-        euler_solution.steps_to_print = 2;
-        std::cout << euler_solution << std::endl;
-
-        // Solve using Explicit Midpoint
-        std::cout << " Explicit Midpoint Method" << std::endl << std::endl;
-        auto midpoint = ExplicitMidpointSolver(scalar_expr, y0, t0, tf, h);
-        auto midpoint_solution = midpoint.solve(); // solve without measuring execution time
-        //auto midpoint_solution = solve_and_measure_execution_time(midpoint); // solve and measure execution time
-        // Print the trajectory using 4 steps
-        midpoint_solution.steps_to_print = 4;
-        std::cout << midpoint_solution << std::endl;
-
-        // Solve using RK4
-        std::cout << " Runge-Kutta 4th Order Method" << std::endl << std::endl;
-        auto rk4 = RK4Solver(scalar_expr, y0, t0, tf, h);
-        auto rk4_solution = rk4.solve(); // solve without measuring execution time
-        //auto rk4_solution = solve_and_measure_execution_time(rk4); // solve and measure execution time
-        // Print the trajectory using 6 steps
-        rk4_solution.steps_to_print = 6;
-        std::cout << rk4_solution << std::endl;
-
-        // Save results to CSV
-        save_to_csv(outputFolder / path("scalar_ode_results_FE.csv") , euler_solution);
-        std::cout << "results saved on output/ODE_Module/scalar_ode_results_FE.csv" << std::endl;
-        save_to_csv(outputFolder / path("scalar_ode_results_EM.csv") , midpoint_solution);
-        std::cout << "results saved on output/ODE_Module/scalar_ode_results_EM.csv" << std::endl;
-        save_to_csv(outputFolder / path("scalar_ode_results_RK4.csv") , rk4_solution);
-        std::cout << "results saved on output/ODE_Module/scalar_ode_results_RK4.csv" << std::endl;
-
-        // Example 2: Vector ODE (predator-prey model)
-        std::cout << std::endl << "Example 2: Vector ODE (Predator-Prey Model)" << std::endl << std::endl;
-        vec_s vector_expr = {"0.1 * y1 - 0.02 * y1 * y2", "-0.3 * y2 + 0.01 * y1 * y2"};
-        
-        // Initial conditions: [prey, predator]
-        vec_d vec_y0(2);
-        vec_y0 << 50.0, 20.0;
-        var_vec vector_y0 = vec_y0;
-
-        //Print problem description
-        std::cout << "  Predator-Prey Model:" << std::endl;
-        std::cout << "    dy1/dt           :\t" << vector_expr[0] << std::endl;
-        std::cout << "    dy2/dt           :\t" << vector_expr[1] << std::endl;
-        std::cout << "    Time interval    :\t[" << t0 << ", " << tf << "]" << std::endl;
-        std::cout << "    Time step        :\t" << h << std::endl;
-        std::cout << "    y(0)             :\t" << vector_y0 << std::endl;
-
-        // Solve using Forward Euler
-        auto vector_euler = ForwardEulerSolver(vector_expr, vector_y0, t0, tf, h);
-        auto vector_euler_solution= vector_euler.solve();
-        vector_euler_solution.steps_to_print = 5;
-        std::cout << vector_euler_solution << std::endl;
-
-        // Solve using Explicit Midpoint
-        auto vector_midpoint = ExplicitMidpointSolver(vector_expr, vector_y0, t0, tf, h);
-        auto vector_midpoint_solution= vector_midpoint.solve();
-        vector_midpoint_solution.steps_to_print = 5;
-        std::cout << vector_midpoint_solution << std::endl;
-
-        // Solve using RK4
-        auto vector_rk4 = RK4Solver(vector_expr, vector_y0, t0, tf, h);
-        auto vector_solution= vector_rk4.solve();
-        vector_solution.steps_to_print = 5;
-        std::cout << vector_solution << std::endl;
-
-        save_to_csv(outputFolder / path("vector_ode_results_FE.csv"), vector_euler_solution);
-        std::cout << "results saved on output/ODE_Module/vector_ode_results_FE.csv" << std::endl;
-        save_to_csv(outputFolder / path("vector_ode_results_EM.csv"), vector_midpoint_solution);
-        std::cout << "results saved on output/ODE_Module/vector_ode_results_EM.csv" << std::endl;
-        save_to_csv(outputFolder / path("vector_ode_results_RK4.csv"), vector_solution);
-        std::cout << "results saved on output/ODE_Module/vector_ode_results_RK4.csv" << std::endl;
-        */
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return 1;
